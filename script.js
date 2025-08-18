3c1c15856791a0f80c6e1ae321a8103eaf0b8d50
FerseJoser <69739134+FerseJoser@users.noreply.github.com>
2025-08-17 20:25:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let fillLevel = 100; // inicia vacío (y=100)
+let fillLevel = 100; 
 let filling = false;
 let interval;
 const fillRect = document.getElementById("fillRect");
@@ -11,9 +11,9 @@ function updateFill() {
   fillRect.setAttribute("height", 100 - fillLevel);
 }
 
-// Inicia el llenado
+
 function startFilling(e) {
-  e.preventDefault(); // evita scroll en móvil
+  e.preventDefault(); 
   filling = true;
   heartSvg.classList.add("pulse");
   setTimeout(() => heartSvg.classList.remove("pulse"), 150);
@@ -24,7 +24,7 @@ function startFilling(e) {
       fillLevel -= 1.5;
       updateFill();
 
-      // Si está lleno → redirige
+      
       if (fillLevel <= 0) {
         window.location.href = "mensaje.html";
       }
@@ -32,7 +32,7 @@ function startFilling(e) {
   }, 20);
 }
 
-// Detiene el llenado y empieza a vaciar
+
 function stopFilling(e) {
   e.preventDefault();
   filling = false;
@@ -45,13 +45,12 @@ function stopFilling(e) {
   }, 20);
 }
 
-// Eventos para PC
+
 const heartContainer = document.querySelector(".heart-container");
 heartContainer.addEventListener("mousedown", startFilling);
 heartContainer.addEventListener("mouseup", stopFilling);
 heartContainer.addEventListener("mouseleave", stopFilling);
 
-// Eventos para móvil
 heartContainer.addEventListener("touchstart", startFilling, { passive: false });
 heartContainer.addEventListener("touchend", stopFilling, { passive: false });
 
