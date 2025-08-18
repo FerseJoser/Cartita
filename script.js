68e8a641429eb03f5d4c76ce4008f974a9583f4e
FerseJoser <FerseJoser@gmail.com>
2025-08-17 19:34:32 -0500

    segundo commit

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ let interval;
 const fillRect = document.getElementById("fillRect");
 const heartSvg = document.getElementById("heartSvg");
 
+
+var layerCount = 5;
+var starCount = 400;
+var maxTime = 30;
+var universe = document.getElementById("universe");
+var w = window;
+var d = document;
+var e = d.documentElement;
+var g = d.getElementsByTagName("body")[0];
+var width = w.innerWidth || e.clientWidth || g.clientWidth;
+var height = w.innerHeight || e.clientHeight || g.clientHeight;
+
+
+
 function updateFill() {
   fillRect.setAttribute("y", fillLevel);
   fillRect.setAttribute("height", 100 - fillLevel);
@@ -19,7 +33,7 @@ function startFilling(e) {
   clearInterval(interval);
   interval = setInterval(() => {
     if (filling && fillLevel > 0) {
-      fillLevel -= 1;
+      fillLevel -= 1.5;
       updateFill();
 
       // Si está lleno → redirige
@@ -52,3 +66,43 @@ heartContainer.addEventListener("mouseleave", stopFilling);
 // Eventos para móvil
 heartContainer.addEventListener("touchstart", startFilling, { passive: false });
 heartContainer.addEventListener("touchend", stopFilling, { passive: false });
+
+
+for (var i = 0; i < starCount; ++i) {
+  var ypos = Math.round(Math.random() * height);
+  var star = document.createElement("div");
+  var speed = 1000 * (Math.random() * maxTime + 1);
+  star.setAttribute("class", "star" + (3 - Math.floor(speed / 1000 / 8)));
+  star.style.backgroundColor = "white";
+
+  universe.appendChild(star);
+  star.animate(
+    [
+      {
+        transform: "translate3d(" + width + "px, " + ypos + "px, 0)"
+      },
+      {
+        transform:
+          "translate3d(-" + Math.random() * 256 + "px, " + ypos + "px, 0)"
+      }
+    ],
+    {
+      delay: Math.random() * -speed,
+      duration: speed,
+      iterations: 1000
+    }
+  );
+}
+
+var elem = document.querySelector(".pulse");
+var animation = elem.animate(
+  {
+    opacity: [0.5, 1],
+    transform: ["scale(0.5)", "scale(1)"]
+  },
+  {
+    direction: "alternate",
+    duration: 500,
+    iterations: Infinity
+  }
+);
